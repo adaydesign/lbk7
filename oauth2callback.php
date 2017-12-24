acdd6f6967cd02b07dc6eaad11da65e0936b03ce
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-12-25 05:59:17 +0700

    +5

--- oauth2callback.php
+++ oauth2callback.php
@@ -1,12 +1,18 @@
 <?php
+/*
+  Oauth callback for heroku server
+  
+  Note !
+  when connect with URL 10.37.84.1 use this callback
+  but if connect with URL localhost use local callback
+*/
 require_once __DIR__.'/vendor/autoload.php';
 
 session_start();
 
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret.json');
-// $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/lbkcbc/oauth2callback.php');
-// $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php'); // v.1.1.94 host > http://www.lbkcbc.org
+
 $client->setRedirectUri('https://lbk7.herokuapp.com/oauth2callback.php'); // use heroku server
 $client->setAccessType("offline");                          // offline access
 $client->setIncludeGrantedScopes(true);                     // incremental auth
@@ -17,11 +23,9 @@ if (! isset($_GET['code'])) {
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
   $client->authenticate($_GET['code']);
-  $_SESSION['access_token'] = $client->getAccessToken();
-  // $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/lbkcbc/work_calendar_manage.php';
-  // $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/work_calendar_manage.php'; // v.1.1.94 host > http://www.lbkcbc.org
   $acc_token = serialize($client->getAccessToken());
-  $redirect_uri = "http://localhost/lbkcbc/work_calendar_manage.php?acc_token=$acc_token";
+
+  $redirect_uri = "http://10.37.84.1/lbkcbc/work_calendar_access.php?token=$acc_token";
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
