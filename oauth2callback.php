eae6d0a5b59a00ebddb811a17c2c457458394b25
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-12-25 04:28:23 +0700

    +2

--- oauth2callback.php
+++ oauth2callback.php
@@ -4,9 +4,10 @@ require_once __DIR__.'/vendor/autoload.php';
 session_start();
 
 $client = new Google_Client();
-$client->setAuthConfigFile('client_secret_2.json');
+$client->setAuthConfigFile('client_secret.json');
 // $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/lbkcbc/oauth2callback.php');
-$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php'); // v.1.1.94 host > http://www.lbkcbc.org
+// $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php'); // v.1.1.94 host > http://www.lbkcbc.org
+$client->setRedirectUri('https://lbk7.herokuapp.com/oauth2callback.php'); // use heroku server
 $client->setAccessType("offline");                          // offline access
 $client->setIncludeGrantedScopes(true);                     // incremental auth
 $client->addScope(Google_Service_Calendar::CALENDAR);
@@ -18,7 +19,8 @@ if (! isset($_GET['code'])) {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
   // $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/lbkcbc/work_calendar_manage.php';
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/work_calendar_manage.php'; // v.1.1.94 host > http://www.lbkcbc.org
+  // $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/work_calendar_manage.php'; // v.1.1.94 host > http://www.lbkcbc.org
+  $redirect_uri = 'http://localhost/lbkcbc/work_calendar_manage.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
